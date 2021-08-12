0189f447a854d5e39b4a2ac7c6606e12be09c4cd
Adnan <adnan.akhter321@gmail.com>
2021-08-12 19:01:09 +0500

    updated

--- Web and Mobile App Development/hakaton preparations/hackathon preparation/supaari  by me/app.js
+++ Web and Mobile App Development/hakaton preparations/hackathon preparation/supaari  by me/app.js
@@ -4,17 +4,32 @@ let passwordEl = document.getElementById('password');
 let passwordRepeatEl = document.getElementById('repeatp');
 let userRoleEl = document.getElementsByName('user-role');
 let userImageEl = document.getElementById('file');
-
-
-function registerUser() {
-    console.log('user');
+let randomimg = document.getElementsByClassName('avatar');
+console.log(randomimg[0]);
+function registeruser() {
     let list = {
-        username = userNameEl.value,
-        Email = emailEl.value,
-        Password = passwordEl.value,
-        RepeatPass = passwordRepeatEl.value,
-        UserRole = userRoleEl.value,
-        userImage  = userImageEl.file[0]
+        username: userNameEl.value,
+        Email: emailEl.value,
+        Password: passwordEl.value,
+        RepeatPass: passwordRepeatEl.value,
+        UserRole: checkrole(userRoleEl),
+        userImage: userImageEl.files[0]
     }
     console.log(list);
+}
+
+
+function checkrole(get) {
+    let checkrole;
+    for (var a = 0; a < get.length; a++) {
+        if (get[a].checked) {
+            checkrole = get[a].value;
+        }
+    }
+    return checkrole;
+}
+
+function updateimg() {
+       console.log(userImageEl.files[0].name);
+       randomimg[0].src = './userImageEl.files[0].name'
 }
