3571d2d301ecdc139270d1d8a77c4e7845e2b718
Adnan <77693222+Adnanakhter321@users.noreply.github.com>
2021-08-14 16:23:39 +0500

    Update 14-8-2021

--- Web and Mobile App Development/hakaton preparations/hackathon preparation/supaari  by me/app.js
+++ Web and Mobile App Development/hakaton preparations/hackathon preparation/supaari  by me/app.js
@@ -1,8 +1,3 @@
-let storage = firebase.storage();
-let auth = firebase.auth();
-let db = firebase.firestore();
-
-
 let userNameEl = document.getElementById('user-name');
 let emailEl = document.getElementById('email');
 let passwordEl = document.getElementById('password');
@@ -10,31 +5,24 @@ let passwordRepeatEl = document.getElementById('repeatp');
 let userRoleEl = document.getElementsByName('user-role');
 let userImageEl = document.getElementById('file');
 let randomimg = document.getElementsByClassName('avatar');
-
-async function registerUser() {
-
-    let userCreated = await auth.createUserWithEmailAndPassword(emailEl.value, passwordEl.value);
-    let UID = userCreated.user.uid;
-    let imageURL = await uploadImageToStorage(UID);
-
-    let user = {
-        userName: userNameEl.value,
-        email: emailEl.value,
-        userImage: imageURL,
-        userRole: giveCheckedRadio(),
-        uid: UID
+function registeruser() {
+    let list = {
+        username: userNameEl.value,
+        Email: emailEl.value,
+        Password: passwordEl.value,
+        RepeatPass: passwordRepeatEl.value,
+        UserRole: checkrole(),
+        userImage: userImageEl.files[0]
     }
-
-    await db.collection('users').doc(UID).set(user);
-
+    console.log(list);
 }
 
 
-function checkrole(get) {
+function checkrole() {
     let checkrole;
-    for (var a = 0; a < get.length; a++) {
-        if (get[a].checked) {
-            checkrole = get[a].value;
+    for (var a = 0; a < userRoleEl.length; a++) {
+        if (userRoleEl[a].checked) {
+            checkrole = userRoleEl[a].value;
         }
     }
     return checkrole;
@@ -42,52 +30,4 @@ function checkrole(get) {
 
 function updateimg() {
        randomimg[0].src =  '../' + userImageEl.files[0].name
-}
-
-
-
-function uploadImageToStorage(UID) {
-    return new Promise(async (resolve, reject) => {
-        let image = userImageEl.files[0];
-        let storageRef = storage.ref();
-        let imageRef = storageRef.child(`avatar/${UID}/${image.name}`);
-        await imageRef.put(image);
-        let url = await imageRef.getDownloadURL();
-        resolve(url);
-    })
-}
-
-
-auth.onAuthStateChanged((user) => {
-    console.log(user);
-    let pageLocArr = window.location.href.split('/');
-    console.log(pageLocArr);
-    let pageName = pageLocArr[pageLocArr.length - 1];
-    let authenticatedPages = ['home.html', 'findwork.html', 'myjob.html'];
-    console.log(authenticatedPages.indexOf(pageName));
-    if (user && authenticatedPages.indexOf(pageName) === -1) {
-        window.location = './findwork.html';
-    }
-//     else if(user == null){
-//    window.location = './index.html';
-//   }
-    else if (!user && pageName === 'home.html') {
-        window.location = './index.html';
-    }
-});
-
-
-
-
-
-
-
-async function signout() {
-    await auth.signOut();
-}
-
-
-
-async function signinUser() {
-    await auth.signInWithEmailAndPassword(emailEl.value, passwordEl.value);
 }
