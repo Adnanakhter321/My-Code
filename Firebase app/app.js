fe2d657854e2e8dd52d371544790fb724fce02b0
Adnan <77693222+Adnanakhter321@users.noreply.github.com>
2021-08-22 01:12:19 +0500

    register and login form completed firebase data complete

--- Firebase app/app.js
+++ Firebase app/app.js
@@ -5,17 +5,17 @@ let passwordRepeatEl = document.getElementById('repeatp');
 let userRoleEl = document.getElementsByName('user-role');
 let userImageEl = document.getElementById('file');
 let country = document.getElementById('country');
-let imgEl  = document.getElementById('file');
-let imgEl2  = document.getElementById('image');
-function changeimage(){
-  imgEl2.src  = imgEl.files[0].name;
+let imgEl = document.getElementById('file');
+let imgEl2 = document.getElementById('image');
+function changeimage() {
+    imgEl2.src = imgEl.files[0].name;
 }
 
-  async  function loginuser(){
-    try{
-        let login = await  firebase.auth().signInWithEmailAndPassword(emailEl.value, passwordEl.value)
-     
-    }catch(error){
+async function loginuser() {
+    try {
+        let login = await firebase.auth().signInWithEmailAndPassword(emailEl.value, passwordEl.value)
+
+    } catch (error) {
         console.log(error);
     }
 }
@@ -34,8 +34,8 @@ async function registeruser() {
     let user = userCredential.user;
 
     console.log('USER CREATED');
-   let url = await imageuploadtofirebase(user.uid);
-   console.log(url);
+    let url = await imageuploadtofirebase(user.uid);
+    console.log(url);
     let users = {
         username: userNameEl.value,
         email: emailEl.value,
@@ -54,11 +54,11 @@ async function registeruser() {
 }
 
 async function imageuploadtofirebase(uid) {
-return new Promise(async (resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         let image = userImageEl.files[0];
         let storageRef = storage.ref();
         let imageRef = storageRef.child(`userimages/${uid}/${image.name}`);
-            await imageRef.put(image)
+        await imageRef.put(image)
         console.log('IMAGE UPLOADED');
         var url = await imageRef.getDownloadURL()
         resolve(url)
@@ -88,28 +88,29 @@ function checkcountry() {
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-      var uid = user.uid;
-      console.log(uid);
-      console.log(user);
-      if(uid &&  window.location == './index.html' &&  window.location == './login.html'){
-        window.location = './home.html';
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        var uid = user.uid;
+        console.log(uid);
+        console.log(user);
+        if (user && window.location === './index.html' || window.location === './login.html' ) {
+            window.location = './home.html';
+        }
+        // ...
     }
-    else{
-      
-    }
-      // ...
-    } else {
-    if(window.location == './index.html'){
-        window.location = './index.html';
-    }
-       
-      // User is signed out
-      // ...
-    }
-  });
 
-  function logout(){
-      firebase.auth().signOut();
-  }
+
+    else {
+
+        if (!user && window.location === './home.html') {
+            window.location = './index.html';
+        }
+
+        // User is signed out
+        // ...
+    }
+});
+
+function logout() {
+    firebase.auth().signOut();
+}
