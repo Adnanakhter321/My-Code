df64a38dc6493bd1145f8ccdd24eea2b7e0abf6c
Adnan <77693222+Adnanakhter321@users.noreply.github.com>
2021-08-21 17:52:15 +0500

    commit

--- Firebase app/app.js
+++ Firebase app/app.js
@@ -6,7 +6,50 @@ let userRoleEl = document.getElementsByName('user-role');
 let userImageEl = document.getElementById('file');
 // let randomimg = document.getElementsByClassName('avatar');
 
-function registeruser(){
-    console.log(userNameEl.value,emailEl.value, passwordEl.value, userRoleEl.value,userImageEl.files );
+function registeruser() {
 
+    let users = {
+        username: userNameEl.value,
+        email: emailEl.value,
+        password: passwordEl.value,
+        Rpassword: passwordRepeatEl.value,
+        userRole: checkrole(),
+        userImage: userImageEl.files[0].name
+    }
+    let db = firebase.firestore();
+
+     firebase.auth().createUserWithEmailAndPassword(emailEl.value, passwordEl.value)
+        .then((userCredential) => {
+            var user = userCredential.user;
+            console.log('done');
+            db.collection('users').doc(user.uid).set(users);
+        })
+
+
+        .catch((error) => {
+            var errorMessage = error.message;
+           console.log(errorMessage);
+        });
+
+
+
+}
+
+// function Details(){
+//     this.username = userNameEl.value;
+//     this.email = emailEl.value;
+//     this.password = passwordEl.value;
+//     this.Rpassword = passwordRepeatEl.value;
+//     this.userRole = checkrole();
+//     this.userImage = userImageEl.files;
+// }
+
+function checkrole() {
+    let checkrole;
+    for (var a = 0; a < userRoleEl.length; a++) {
+        if (userRoleEl[a].checked) {
+            checkrole = userRoleEl[a].value;
+        }
+    }
+    return checkrole;
 }
