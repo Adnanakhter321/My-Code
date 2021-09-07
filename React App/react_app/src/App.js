9938ea5545d7814b41f5fbd17d8064dbd0776f10
Adnan <77693222+Adnanakhter321@users.noreply.github.com>
2021-09-07 19:49:59 +0500

    Addoing bootstarp with more than one components and linking

--- React App/react_app/src/App.js
+++ React App/react_app/src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 
 import Navbar from './components/Navbar'
+import Pricing from './components/Pricing'
 
-export default function Greeting(prop) {
+export default function Greeting() {
   return (
     <>
-    <Navbar title='GeTServices' hd = 'Contact Us'/>
-    {/* <Navbar title='GeTServices'/> */}
+      <Navbar title='GeTServices' hd='Contact Us' />
+      {/* <Navbar title='GeTServices'/> */}
+
+
+      {/* passing components  */}
+      <Pricing />
+      {/* passing components  */}
+
+
     </>
   )
 }
+
+// export function Price(){
+//   return (
+//     <>
+//     <Pricing/>
+//     </>
+//   )
+// }
