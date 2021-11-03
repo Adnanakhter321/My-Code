935775f416ae87d9c8f3ef0c2d5a6e32a4b46995
Adnan <77693222+Adnanakhter321@users.noreply.github.com>
2021-11-03 19:21:16 +0500

    Todo_App with React ReduX Complete

--- food_website_react/src/components/SignUp.js
+++ food_website_react/src/components/SignUp.js
@@ -12,126 +12,143 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {Link as Redirect} from 'react-router-dom'
+import { Link as Redirect } from 'react-router-dom'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+// import { logRoles } from '@testing-library/dom';
+import {  doc, setDoc, getFirestore} from "firebase/firestore"
 function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit">
-      FooDHuB
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit">
+                FooDHuB
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
 }
 
 const theme = createTheme();
 
 export default function SignUp() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
+    let User;
+    const handleSubmit = (event) => {
+        let db = getFirestore()
+        const data = new FormData(event.currentTarget);
+        event.preventDefault();
+        console.log('hi');
+        // eslint-disable-next-line no-console
+        User = {
+            firstName : data.get('firstName'),
+            lastName:data.get('lastName') ,
+            Email : data.get('email'),
+            password : data.get('password'),
+        }
+        const auth = getAuth();
+        
+        createUserWithEmailAndPassword(auth, User.Email, User.password)
+          .then( async (userCredential) => {
+            const user = userCredential.user.uid;
+            User.uid = user;
+            await setDoc(doc(db, "Users", User.uid), User);
+        })
+    };
+    
 
-  return (
+return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            backgroundColor: "#f9f9f9",
-            padding: "1rem 1rem 1rem 1rem",
-            borderRadius: '2rem',
-          }}
-        >
-          
-          <div>
-          <img style={{width:100}} src="https://mydeliveryappadnan.web.app/images/logo2.png" alt="F" />
-          </div>
-          <Typography component="h1" variant="h5">
-            Sign up For FooDHuB
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    backgroundColor: "#f9f9f9",
+                    padding: "1rem 1rem 1rem 1rem",
+                    borderRadius: '2rem',
+                }}
             >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Redirect to='/SignIn'>
-                <Link  variant="body2">
-                  Already have an account? Sign in
-                </Link></Redirect>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
+
+                <div>
+                    <img style={{ width: 100 }} src="https://mydeliveryappadnan.web.app/images/logo2.png" alt="F" />
+                </div>
+                <Typography component="h1" variant="h5">
+                    Sign up For FooDHuB
+                </Typography>
+                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                autoComplete="given-name"
+                                name="firstName"
+                                required
+                                fullWidth
+                                id="firstName"
+                                label="First Name"
+                                autoFocus
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="lastName"
+                                label="Last Name"
+                                name="lastName"
+                                autoComplete="family-name"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="new-password"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControlLabel
+                                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                                label="I want to receive inspiration, marketing promotions and updates via email."
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Sign Up
+                    </Button>
+                    <Grid container justifyContent="flex-end">
+                        <Grid item>
+                            <Redirect to='/SignIn'>
+                                <Link variant="body2">
+                                    Already have an account? Sign in
+                                </Link></Redirect>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+            <Copyright sx={{ mt: 5 }} />
+        </Container>
     </ThemeProvider>
-  );
+);
 }
--- food_website_react/src/configs/Firebase.js
+++ food_website_react/src/configs/Firebase.js
@@ -1,13 +1,8 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged , signOut} from "firebase/auth";
-
 import { doc, setDoc ,getFirestore , getDocs ,collection, getDoc,addDoc , onSnapshot ,query ,where,updateDoc} from "firebase/firestore"; 
-let db = getFirestore();
-  
-const auth = getAuth();
 
-const firebaseConfig = initializeApp({
+initializeApp({
   apiKey: "AIzaSyBPzzBEOz9ek3xxSNpJvVLSG_-dNWsvwPg",
   authDomain: "foodappreact-a4c53.firebaseapp.com",
   projectId: "foodappreact-a4c53",
@@ -15,9 +10,12 @@ const firebaseConfig = initializeApp({
   messagingSenderId: "1012321487120",
   appId: "1:1012321487120:web:b4a7ffdb9eb1b308fdbd95",
   measurementId: "G-XE9MVXWNM1"
-});
+  });
 
-const analytics = getAnalytics(firebaseConfig);
+let db = getFirestore();
+  
+const auth = getAuth();
+
 export {
     auth,
     createUserWithEmailAndPassword,
@@ -30,10 +28,7 @@ export {
     collection,
     getDocs,
     db,
-    getDoc,
-    addDoc,
-    onSnapshot,
-    query,
-    where,
-    updateDoc
+    getDoc,addDoc
+    ,onSnapshot,
+    query,where,updateDoc
 };
